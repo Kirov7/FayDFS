e86462c41724c889e919aedbd6163a5c3bcf72c7
25222 <2522235302@qq.com>
2022-08-12 23:18:38 +0800

    修改config.go相对路径的bug

--- config/config.go
+++ config/config.go
@@ -2,9 +2,9 @@ package config
 
 import (
 	"encoding/json"
-	"faydfs/public"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // util for parsing configs json
@@ -22,20 +22,38 @@ type Config struct {
 	HeartbeatInterval   int
 	HeartbeatTimeout    int
 	BlockReportInterval int
+	LeaseSoftLimit      int
+	LeaseHardLimit      int
 }
+
+var Conf *Config
 
 // GetConfig parses config.json and returns
 // Config struct
-func GetConfig() Config {
-	var config Config
+func GetConfig() *Config {
+	return Conf
+}
+
+func init() {
+
+	var curPath string
+	for {
+		curPath, _ = os.Getwd()
+		curPathList := strings.Split(curPath, "\\")
+		curPath = curPathList[len(curPathList)-1]
+		if curPath == "FayDFS" {
+			break
+		}
+		os.Chdir("../")
+	}
+
 	file := "./config/config.json"
+
 	configFile, err := os.Open(file)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	defer configFile.Close()
 	jsonParser := json.NewDecoder(configFile)
-	jsonParser.Decode(&config)
-	public.CreateDir(config.DataDir)
-	return config
+	jsonParser.Decode(&Conf)
 }
