635fe048a48b28d528f8690670295a18fdd7dc1f
CrazyFay <2522235302@qq.com>
2022-09-08 23:33:47 +0800

    change some variable Names

--- namenode/service/leveldb.go
+++ namenode/service/leveldb.go
@@ -31,58 +31,58 @@ func GetDB(dbPath string) *DB {
 	return &DB{DB: db, Size: length}
 }
 
-func (fm *DB) GetDnSize() int {
-	return fm.Size
+func (db *DB) GetDnSize() int {
+	return db.Size
 }
 
-func (fm *DB) Put(key string, value *FileMeta) {
-	valueBytes := fm.data2Bytes(value)
-	err := fm.DB.Put([]byte(key), valueBytes, nil)
+func (db *DB) Put(key string, value *FileMeta) {
+	valueBytes := db.data2Bytes(value)
+	err := db.DB.Put([]byte(key), valueBytes, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func (fm *DB) Get(key string) (*FileMeta, bool) {
-	data, err := fm.DB.Get([]byte(key), nil)
+func (db *DB) Get(key string) (*FileMeta, bool) {
+	data, err := db.DB.Get([]byte(key), nil)
 	if err != nil {
 		if err == leveldb.ErrNotFound {
 			return nil, false
 		}
 		log.Fatal(err)
 	}
-	result := fm.bytes2FileMetas(data)
+	result := db.bytes2FileMetas(data)
 	return result, true
 }
 
-func (fm *DB) GetValue(key string) *FileMeta {
-	data, err := fm.DB.Get([]byte(key), nil)
+func (db *DB) GetValue(key string) *FileMeta {
+	data, err := db.DB.Get([]byte(key), nil)
 	if err != nil {
 		if err == leveldb.ErrNotFound {
 			return nil
 		}
 		log.Fatal(err)
 	}
-	result := fm.bytes2FileMetas(data)
+	result := db.bytes2FileMetas(data)
 	return result
 }
 
-func (fm *DB) Delete(key string) {
-	err := fm.DB.Delete([]byte(key), nil)
+func (db *DB) Delete(key string) {
+	err := db.DB.Delete([]byte(key), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func (fm *DB) Range() ([]string, []*FileMeta) {
+func (db *DB) Range() ([]string, []*FileMeta) {
 	keys := []string{}
 	values := []*FileMeta{}
-	iter := fm.DB.NewIterator(nil, nil)
+	iter := db.DB.NewIterator(nil, nil)
 	for iter.Next() {
 		key := iter.Key()
 		keys = append(keys, string(key))
 		value := iter.Value()
-		values = append(values, fm.bytes2FileMetas(value))
+		values = append(values, db.bytes2FileMetas(value))
 	}
 	iter.Release()
 	err := iter.Error()
@@ -92,37 +92,37 @@ func (fm *DB) Range() ([]string, []*FileMeta) {
 	return keys, values
 }
 
-func (fm *DB) UpdateDn(value map[string]*DatanodeMeta) {
-	valueBytes := fm.data2Bytes(value)
-	err := fm.DB.Put([]byte("dnList"), valueBytes, nil)
+func (db *DB) UpdateDn(value map[string]*DatanodeMeta) {
+	valueBytes := db.data2Bytes(value)
+	err := db.DB.Put([]byte("dnList"), valueBytes, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func (fm *DB) AddDn(value map[string]*DatanodeMeta) {
-	valueBytes := fm.data2Bytes(value)
-	err := fm.DB.Put([]byte("dnList"), valueBytes, nil)
+func (db *DB) AddDn(value map[string]*DatanodeMeta) {
+	valueBytes := db.data2Bytes(value)
+	err := db.DB.Put([]byte("dnList"), valueBytes, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fm.Size++
+	db.Size++
 }
 
-func (fm *DB) GetDn() map[string]*DatanodeMeta {
-	data, err := fm.DB.Get([]byte("dnList"), nil)
+func (db *DB) GetDn() map[string]*DatanodeMeta {
+	data, err := db.DB.Get([]byte("dnList"), nil)
 	if err != nil {
 		if err == leveldb.ErrNotFound {
 			return nil
 		}
 		log.Fatal(err)
 	}
-	result := fm.bytes2DatanodeMetas(data)
+	result := db.bytes2DatanodeMetas(data)
 	return result
 }
 
-func (fm *DB) RaftGet(key []byte) ([]byte, error) {
-	data, err := fm.DB.Get(key, nil)
+func (db *DB) RaftGet(key []byte) ([]byte, error) {
+	data, err := db.DB.Get(key, nil)
 	if err != nil {
 		if err == leveldb.ErrNotFound {
 			return nil, nil
@@ -132,18 +132,18 @@ func (fm *DB) RaftGet(key []byte) ([]byte, error) {
 	return data, nil
 }
 
-func (fm *DB) RaftPut(key []byte, state []byte) error {
-	return fm.DB.Put(key, state, nil)
+func (db *DB) RaftPut(key []byte, state []byte) error {
+	return db.DB.Put(key, state, nil)
 }
 
-func (fm *DB) RaftDelete(key []byte) error {
-	return fm.DB.Delete(key, nil)
+func (db *DB) RaftDelete(key []byte) error {
+	return db.DB.Delete(key, nil)
 }
 
-func (fm *DB) RaftDelPrefixKeys(prefix string) error {
-	iter := fm.DB.NewIterator(util.BytesPrefix([]byte(prefix)), nil)
+func (db *DB) RaftDelPrefixKeys(prefix string) error {
+	iter := db.DB.NewIterator(util.BytesPrefix([]byte(prefix)), nil)
 	for iter.Next() {
-		err := fm.DB.Delete(iter.Key(), nil)
+		err := db.DB.Delete(iter.Key(), nil)
 		if err != nil {
 			return err
 		}
@@ -152,7 +152,7 @@ func (fm *DB) RaftDelPrefixKeys(prefix string) error {
 	return nil
 }
 
-func (fm *DB) data2Bytes(structs interface{}) []byte {
+func (db *DB) data2Bytes(structs interface{}) []byte {
 	var b bytes.Buffer
 	enc := gob.NewEncoder(&b)
 	err := enc.Encode(structs)
@@ -162,7 +162,7 @@ func (fm *DB) data2Bytes(structs interface{}) []byte {
 	return b.Bytes()
 }
 
-func (fm *DB) bytes2FileMetas(b []byte) *FileMeta {
+func (db *DB) bytes2FileMetas(b []byte) *FileMeta {
 	dec := gob.NewDecoder(bytes.NewBuffer(b))
 	var data *FileMeta
 	err := dec.Decode(&data)
@@ -172,7 +172,7 @@ func (fm *DB) bytes2FileMetas(b []byte) *FileMeta {
 	return data
 }
 
-func (fm *DB) bytes2DatanodeMetas(b []byte) map[string]*DatanodeMeta {
+func (db *DB) bytes2DatanodeMetas(b []byte) map[string]*DatanodeMeta {
 	dec := gob.NewDecoder(bytes.NewBuffer(b))
 	var data map[string]*DatanodeMeta
 	err := dec.Decode(&data)
